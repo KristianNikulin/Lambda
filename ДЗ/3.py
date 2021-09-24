aefc998e16917b6662fec4a28368a8607136cd47
Kristian Nikulin <88029785+KristianNikulin@users.noreply.github.com>
2021-09-24 15:05:59 +0300

    Delete ДЗ directory

--- ДЗ/3.py
+++
@@ -1,7 +0,0 @@
-a = []
-summ = 0
-for i in range(1, 11):
-    a.append( int(input()) )
-for i in range(len(a)):
-    summ += a[i]
-print(summ)
--- ДЗ/4.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-a = []
-col = 0
-for i in range(N):
-    a.append( int(input()) )
-for i in range(len(a)):
-    if a[i] == 0:
-        col += 1
-print(col)
